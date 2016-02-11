2d37c14aded3ad945d82c3117111397f0a5cbeb2
Kent Nagata <ngtknt@me.com>
2016-02-11 22:21:53 +0900

    minimal code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='practices',
+      version='0.1',
+      description='python practices',
+      url='http://github.com/ngtk-practices/practice-python',
+      author='Kent Nagata',
+      author_email='ngtknt@me.com',
+      license='MIT',
+      packages=['practices'],
+      zip_safe=False)
---
+++ tests/test_sample.py
@@ -0,0 +1,5 @@
+import pytest
+from practices import func
+
+def test_func():
+    assert func(3) == 4
